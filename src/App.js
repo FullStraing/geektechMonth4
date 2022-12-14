0600c388e5971e5a4b442bdbf4f7499b4b6b7a88
flex-oops <94053950+FullStraing@users.noreply.github.com>
2022-12-14 18:57:42 +0600

    Добавил 2 ДЗ

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+// import { useState } from "react";
+import Counter from "./components/Counter/Counter";
+
+function App() {
+  return(
+    <div className="App">
+      <Counter/>
+    </div>
+  )
+}
+
+export default App;
