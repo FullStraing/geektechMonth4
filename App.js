ad6b4df7e01e6c1117c4d8e92b2068f4179c4d77
flex-oops <94053950+FullStraing@users.noreply.github.com>
2023-01-05 12:46:32 +0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
-import React, { useState } from "react";
-// import { useState } from "react";
-import Counter from "./components/Counter/Counter";
+import React,{useMemo, useState} from 'react'
+import MainList from './components/MainList';
+import MainForm from './components/MainForm';
+import './styles/App.css'
+import Input from './components/UI/Input';
+import Select from './components/UI/Select';
+import { usePosts } from './hooks/usePosts';
+
 
 function App() {
-  return(
+  const [posts, setPosts] = useState([
+    {id: 1, title:'Найти', desc: 'чтоделать'},
+    {id: 2, title:'потерять', desc: 'машина'},
+  ])
+
+  const createNewPost=(newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post)=> {
+    setPosts(posts.filter(p=> p.id !== post.id))
+  }
+  const [filter, setFilter] = useState('')
+  const [selectedSort, setSelectedSort] = useState('')
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  
+  const sortPosts = (sort) =>{
+    setSelectedSort(sort)
+  }
+  return (
     <div className="App">
-      <Counter/>
+      <MainForm  create={createNewPost}/>
+      <div>
+          <Input
+              value={search}
+              onChange={e => setSearch(e.target.value)}
+              placeholder="Поиск..."
+          />
+          <Select
+              value={selectedSort}
+              onChange={sortPosts}
+              defaultValue="Сортировка"
+              options={[
+                  {value: 'title', name: 'По названию'},
+                  {value: 'body', name: 'По описанию'},
+              ]}
+          />
+        </div>
+      <MainList remove={removePost} posts={sortedAndSearchedPosts}/>
     </div>
-  )
+  );
 }
 
-export default App;
+export default App
